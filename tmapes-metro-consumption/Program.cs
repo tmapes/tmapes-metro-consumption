6bc83d119e21f954aa8348f30a49cf90fa8ce8bc
Taylor Mapes <Taylor.Mapes@target.com>
2018-06-14 21:05:23 -0500

    Add project files.

---
+++ tmapes-metro-consumption/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tmapes_metro_consumption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
